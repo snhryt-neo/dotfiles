f9de5cbe91300638546369bc99b820248832ebbc
snhryt-neo <68265677+snhryt-neo@users.noreply.github.com>
2025-05-15 22:50:50 +0900

    🐛 asdf global が使えなくなっていたので新しい書き方に変更
    https://qiita.com/kentrino/items/7911c9184daf1cda2661

--- other.sh
+++ other.sh
@@ -13,7 +13,7 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 # asdfで最新のPythonをインストール＆グローバルなバージョンに設定
 asdf plugin add python
 asdf install python latest
-asdf global python "$(asdf list python | sed 's/  //')"
+asdf set --home python "$(asdf list python | sed 's/  //')"
 
 # =============================================================================
 # Node.js実行環境の構築
@@ -21,7 +21,7 @@ asdf global python "$(asdf list python | sed 's/  //')"
 # asdfで最新のNode.jsをインストール＆グローバルなバージョンに設定
 asdf plugin add nodejs
 asdf install nodejs latest
-asdf global nodejs "$(asdf list nodejs | sed 's/  //')"
+asdf set --home nodejs "$(asdf list nodejs | sed 's/  //')"
 
 # brew ではなく npm でいれるべきツールをインストール
 npm i -g @openai/codex
