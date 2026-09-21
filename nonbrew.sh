9888e46fafa4baa159ee9f005393111c8e98879a
snhryt-neo <68265677+snhryt-neo@users.noreply.github.com>
2026-09-21 16:29:34 +0900

    chore: Context Hubの導入を廃止

--- nonbrew.sh
+++ nonbrew.sh
@@ -15,8 +15,5 @@ echo "📦 Installing global npm packages..."
 # https://github.com/dataform-co/dataform
 npm install -g @dataform/cli
 
-# https://github.com/andrewyng/context-hub
-npm install -g @aisuite/chub
-
 # https://github.com/helpfeel/cosense-cli
 npm install -g @helpfeel/cosense-cli
